42e6ca9a1e027bd47dc4e4b6555bc9a9481f2028
TeamEnternode <20007583j@gmail.com>
2017-02-23 16:49:17 -0500

    Basic Functionality 2

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,20 @@
 #include "Polynomial.h"
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
-string handle_input(string s, int i);
+int getcmd(const string& s);
+
+string expression_simplify(string s);
+string variable_define(string s);
+string polynomial_condense(string s);
+string polynomial_divide(string s);
+string polynomial_factor(string s);
 
 enum Commands {
+	invalid,
 	simplify,
 	define,
 	poly_condense,
@@ -17,13 +25,31 @@ enum Commands {
 int main() {
 	string s;
 	while (getline(cin, s)) {
-		//determine command, then pass arguments
-		int i = 0;
-		cout << handle_input(s, i) << endl;
+		if (s.empty())
+			continue;
+		switch (int i = getcmd(s)) {
+		case simplify:
+			//
+			break;
+		}
+
 	}
 	return 0;
 }
 
-string handle_input(string s, int i) {
-	return "It works!";
+int getcmd(const string& s) {
+	char c = s[0];
+	string sub("");
+	int count = 0;
+	while (!isspace(c)) {
+		sub += c;
+		count++;
+		c = s[count];
+	}
+	if (sub == "simplify") return simplify;
+	else if (sub == "define") return define;
+	else if (sub == "poly_condense") return poly_condense;
+	else if (sub == "poly_divide") return poly_divide;
+	else if (sub == "poly_factor") return poly_factor;
+	else return invalid;
 }
