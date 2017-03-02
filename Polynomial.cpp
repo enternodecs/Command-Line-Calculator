b2f8f4291f7a566bd3a5faea91470265d45077b0
TeamEnternode <20007583j@gmail.com>
2017-03-01 20:40:52 -0500

    Polynomial Division 1
    -Added functionalities required to divide polynomials

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,10 +1,16 @@
 #include "Polynomial.h"
 #include <string>
 #include <cstdlib>
+#include <iostream>
+
+#define DEBUG
 
 using namespace std;
 
 Polynomial::Polynomial(string s) {
+#ifndef DEBUG
+	cout << "Constructing polynomial " << s << endl;
+#endif
 	string::iterator iter = s.begin();
 	string coetemp("");
 	string exptemp("");
@@ -53,6 +59,7 @@ Polynomial::Polynomial(string s) {
 		}
 		//We don't need to bother with plus signs
 		else if (*iter == '+') iter++;
+		else throw exception("Unknown char" + *iter);
 	}
 	//The coefficient should still not have been written but have been read into the temp string
 	d = atof(coetemp.c_str());
@@ -71,7 +78,52 @@ Polynomial::Polynomial(string s) {
 string Polynomial::get() {
 	string ret("");
 	for (int i = degree; i > 0; i--)
+//		if (abs(poly[i] - 0.0) > 1e-7)
 		ret += (to_string(poly[i]) + "x^" + to_string(i) + '+');
 	ret += (to_string(poly[0]));
 	return ret;
+}
+
+string Polynomial::get() const {
+	string ret("");
+	for (int i = degree; i > 0; i--)
+		//		if (abs(poly[i] - 0.0) > 1e-7)
+		ret += (to_string((*this)[i]) + "x^" + to_string(i) + '+');
+	ret += (to_string((*this)[0]));
+	return ret;
+}
+
+//Overloaded * operator that takes polynomial and a pair<int,double> and returns a polynomial
+
+Polynomial operator*(const Polynomial &poly, const pair<int, double> &p) {
+#ifndef DEBUG
+	cout << "Multiplying " << poly.get() << " and " << p.second << "x^" << p.first << endl;
+#endif
+	//Best way to construct polynomial is by using a string
+	string s("");
+	//Variables we do not need to destroy and remake at every iteration
+	double tco;
+	int texp;
+	//For every term in the polynomial
+	for (int i = poly.getDegree(); i != -1; i--) {
+		//Add exponents
+		texp = p.first + i;
+		//Multiply coefficients
+		tco = p.second * poly[i];
+		//Append string
+		if (texp != 0) {
+			s += to_string(tco) + "x^" + to_string(texp) + "+";
+		}
+		else {
+			s += to_string(tco);
+		}
+	}
+	//Return a constructed polynomial from string
+
+	Polynomial answer(s);
+#ifndef DEBUG
+	cout << "Result is " << answer.get() << endl;
+#endif
+
+	return answer;
 }
--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,21 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-
+#include <utility>
+#define DEBUG
 
 //typedef string (*CalcFunc)(string); map<string, CalcFunc>
 using namespace std;
 
 int getcmd(string& s);
 
+//Function declarations
 string expression_simplify(string s);
 string variable_define(string s);
 string polynomial_condense(string s);
 string polynomial_divide(string s);
 string polynomial_factor(string s);
+Polynomial operator*(const Polynomial &poly, const pair<int, double> &p);
 
 enum Commands {
 	invalid,
@@ -31,33 +34,37 @@ int main() {
 	while (getline(cin, s)) {
 		if (s.empty())
 			continue;
-		switch (int i = getcmd(s)) {
-		case simplify:
-			cout << expression_simplify(s) << endl;
-			break;
-		case define:
-			cout << variable_define(s) << endl;
-			break;
-		case poly_condense:
-			cout << polynomial_condense(s) << endl;
-			break;
-		case poly_divide:
-			cout << polynomial_divide(s) << endl;
-			break;
-		case poly_factor:
-			cout << polynomial_factor(s) << endl;
-			break;
-		case close:
-			return 0;
-			break;
-		case help:
-			cout << "Avalible commands: \nsimplify \[expression\]\ndefine \[letter\] \[numer\]" << endl;
-			break;
-		case invalid:
-			cout << "Invalid command." << endl;
+		try {
+			switch (int i = getcmd(s)) {
+			case simplify:
+				cout << expression_simplify(s) << endl;
+				break;
+			case define:
+				cout << variable_define(s) << endl;
+				break;
+			case poly_condense:
+				cout << polynomial_condense(s) << endl;
+				break;
+			case poly_divide:
+				cout << polynomial_divide(s) << endl;
+				break;
+			case poly_factor:
+				cout << polynomial_factor(s) << endl;
+				break;
+			case close:
+				return 0;
+				break;
+			case help:
+				cout << "Avalible commands: \nsimplify [expression]\ndefine [letter] [number]" << endl;
+				break;
+			case invalid:
+				cout << "Invalid command." << endl;
+			}
 		}
+		catch (exception err){
+			cout << err.what() << endl;
+		};
 	}
-	return 0;
 }
 
 int getcmd(string& s) {
@@ -113,9 +120,8 @@ string polynomial_divide(string s) {
 	string::iterator iter = s.begin();
 	if (iter == s.end()) throw exception("No arguments provided to polynomial_divide!");
 	if (*iter == '/') throw exception("Dividend not provided to polynomial_divide!");
-	while (iter != s.end()) {
-		if (*iter != '/') dividend += *iter;
-		else break;
+	while (iter != s.end() && *iter != '/') {
+		dividend += *iter;
 		iter++;
 	}
 	++iter;
@@ -123,8 +129,48 @@ string polynomial_divide(string s) {
 		divisor += *iter;
 		iter++;
 	}
-	Polynomial poly_dividend(dividend), poly_divisor(divisor);
-	return poly_dividend.get() + "\n" + poly_divisor.get();
+	Polynomial poly_dividend(dividend), poly_divisor(divisor), answer("0"), temp("0");
+
+	int exp = 0;
+	double co = 0;
+	string remainder;
+	//Perhaps later this will become an overloaded operator
+	while (poly_divisor.getDegree() <= poly_dividend.getDegree()) {
+		//Step 1: Find how to multiply the first term of the divisor to get the first term of the dividend
+		//We find the exponent of the first term of the answer by subtracting the degrees of the two polynomials
+		exp = poly_dividend.getDegree() - poly_divisor.getDegree();
+		//We then find the coefficient by finding what we multiply the divisor by to get the dividend
+		//We find this by dividing the dividend by the divisor
+		//If we end up dividing by zero it's because the user specified the degree to be zero. Should throw a logic_error exception
+		co = poly_dividend[poly_dividend.getDegree()] / poly_divisor[poly_divisor.getDegree()];
+
+		answer += make_pair(exp, co);
+
+#ifndef DEBUG
+		cout << "Answer is now " << answer.get() << endl;
+#endif
+		
+		//Step 2: Multiply the newfound answer by all terms of divisor
+		//Use overloaded * operator that takes polynomial and a pair<int,double> and returns a polynomial
+
+		temp = (poly_divisor * make_pair(exp, co));
+#ifndef DEBUG
+		cout << "Temp is now " << temp.get() << endl;
+#endif
+
+		//Step 3: Subtract the polynomial from temp
+		//Since we don't care about saving the dividend we'll just edit it
+
+		poly_dividend -= temp;
+#ifndef DEBUG
+		cout << "Poly_Dividend is now " << poly_dividend.get() << endl;
+#endif
+
+		//Step 4: repeat until the degree of the divsor is less than the degree of the dividend
+	}
+	//Now whatever is left in dividend is the remainder
+	remainder = poly_dividend.get();
+	return answer.get() + " R " + remainder;
 }
 
 string polynomial_factor(string s) {
@@ -132,3 +178,4 @@ string polynomial_factor(string s) {
 	Polynomial p(s);
 	return p.get();
 }
+
